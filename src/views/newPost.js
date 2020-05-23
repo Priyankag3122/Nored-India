f287b854db1c01d6fac16eae27f6234bfd8f656b
Priyankag3122 <priyankagangurde31@gmail.com>
2020-05-23 14:25:35 +0530

    Added new post file

---
+++ src/views/newPost.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class newPost extends Component {
+    render() {
+        return (
+            <div>
+                newPost
+            </div>
+        )
+    }
+}
